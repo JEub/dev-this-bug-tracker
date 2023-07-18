f8c4e3f9aa9b634cbda6738f467965696659f76a
Charles Taylor <charles4312@comcast.net>
2023-07-18 19:06:31 -0400

    Removed Unnecessary Class Styles

--- frontend/dev-this-bug-tracker-frontend/src/components/LoginForm/LoginForm.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/LoginForm/LoginForm.vue
@@ -1,14 +1,17 @@
-<script setup></script>
+<script setup>
+// Figure out functionality for logining in. Also decide if the functions will be located in a different file
+
+</script>
 
 <template>
   <form class="authentication-form">
     <div class="authentication-information">
       <div class="authentication-input">
-        <h2>Email</h2>
+        <h2 class="authentication-header">Email</h2>
         <input class="authentication-input-field" placeholder="Email" />
       </div>
       <div class="authentication-input">
-        <h2>Password</h2>
+        <h2 class="authentication-header">Password</h2>
         <input class="authentication-input-field" placeholder="Password" type="password" />
       </div>
     </div>
