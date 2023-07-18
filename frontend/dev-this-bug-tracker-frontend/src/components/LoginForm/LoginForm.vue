354b636648ed42d8ec502233996a68a8fc3e4358
Charles Taylor <charles4312@comcast.net>
2023-07-17 22:14:42 -0400

    Created Initial LoginView And LoginForm.vue And LoginForm.css + Created Route To LoginView

---
+++ frontend/dev-this-bug-tracker-frontend/src/components/LoginForm/LoginForm.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <form class="authentication-form">
+    <div class="authentication-information">
+      <div class="authentication-input">
+        <h2>Email</h2>
+        <input class="authentication-input-field" placeholder="Email"/>
+      </div>
+      <div class="authentication-input">
+        <h2>Password</h2>
+        <input class="authentication-input-field" placeholder="Password" type="password"/>
+      </div>
+    </div>
+    <div class="buttons-section">
+        <div class="buttons primary">
+          <button class="button login-button">Login</button>
+        </div>
+        <div class="buttons secondary">
+            <button class="button password-button">Forgot Password</button>
+            <button class="button register-button">Register</button>
+        </div>
+    </div>
+  </form>
+</template>
+
+<style>
+  @import './LoginForm.css';
+</style>
---
+++ frontend/dev-this-bug-tracker-frontend/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<!-- This is the initial template where I will add components to -->
+<!-- All of the components needed for this page should just be a login form according to the wireframe -->
+
+<script setup>
+    import LoginForm from '../components/LoginForm/LoginForm.vue';
+</script>
+
+<template>
+  <main>
+    <LoginForm />
+  </main>
+</template>
