b8adc998ad67927bb2a6a03de63fdd35d6c131d6
Eric <ettrudell17@gmail.com>
2023-07-18 19:37:39 -0600

    ticket raw template

--- frontend/bug_tracker/src/components/Dashboard.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    import WelcomeItem from './WelcomeItem.vue'
-</script>
-
-<template>
-    <WelcomeItem>
-        <h1></h1>
-    </WelcomeItem>
-</template>
---
+++ frontend/dev-this-bug-tracker-frontend/src/components/Tickets/CreateEditTicket.vue
@@ -0,0 +1,60 @@
+<script setup>
+    //functions in separate file to import?
+</script>
+
+<template>
+    {# entire form will require prepopulate of data from database upon edit function #}
+    <form action="">
+        <div>
+            {# minimum required #}
+            <label for="">Title</label>
+            {# enter values for editing form #}
+            <input type="text"/>
+            <label for="">Description</label>
+            {# enter values for editing form #}
+            <input type="text"/>
+            <label for="">Story Points / Content</label>
+            {# enter values for editing form #}
+            <input/>
+        </div>
+        <div>
+            <label for="">Ticket Status</label>
+            {# stored ticket status from db? #}
+            <select >
+                <option>Open</option>
+                <option>In Progress</option>
+                <option>Blocked</option>
+                <option>Need More Info</option>
+                <option>Closed</option>
+                <option>Cancelled</option>
+            </select>
+            <label for="">Groups</label>
+            <radio>
+                {# call groups from database here #}
+                <option></option>
+            </radio>
+            <label for="">Assigned User</label>
+            <radio>
+                {# call users from database here #}
+                <option></option>
+            </radio>
+            <label for="">Parent Links</label>
+            {# link parent tickets with relationship in database #}
+            <ul>
+                <li></li>
+            </ul>
+            <label for="">Ticket Tags</label>
+            {# call ticket labels from database #}
+            <ul>
+                <li></li>
+            </ul>
+            <label for="">Ticket Comments</label>
+            {# call ticket comments from database #}
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <div>
+                <button type="submit">Submit / Update</button>
+            </div>
+        </div>
+    </form>
+
+</template>
