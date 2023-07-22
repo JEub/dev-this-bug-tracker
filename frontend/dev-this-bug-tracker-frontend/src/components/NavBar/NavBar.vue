7af1e831793489ee7e1faae16748357ccf44f3f8
Charles Taylor <charles4312@comcast.net>
2023-07-22 11:48:49 -0400

    Created Basic NavBar HTML Template

---
+++ frontend/dev-this-bug-tracker-frontend/src/components/NavBar/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- Temporary -->
+    <div>
+        <button>Create Ticket</button>
+        <button>Create User</button>
+    </div>
+    <div>
+        <h2>Welcome, User</h2>
+        <div>
+            <img />
+            <a>Logout</a>
+        </div>
+    </div>
+</template>
