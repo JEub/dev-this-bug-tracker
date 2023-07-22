af9f363beb48bce6a63d747d459790ba89a68b00
Charles Taylor <charles4312@comcast.net>
2023-07-22 12:37:53 -0400

    Created DashBoard.vue And DashBoard.css + Created Table Header For Dashboard Table

---
+++ frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <button>All</button>
+                </td>
+                <td>
+                    <button>Mine</button>
+                </td>
+                <td>
+                    <input placeholder="Search"/>
+                </td>
+                <td>
+                    <button>Filter</button>
+                </td>
+            </tr>
+            <!-- Create a mapping function to display tickets retrieved from the database -->
+        </table>
+        
+    </div>
+</template>
+
+<style>
+
+</style>
--- frontend/dev-this-bug-tracker-frontend/src/views/HomeView.vue
+++ frontend/dev-this-bug-tracker-frontend/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 import NavBar from '../components/NavBar/NavBar.vue'
+import DashBoard from '../components/DashBoard/DashBoard.vue'
 </script>
+
 
 <template>
 
   <NavBar />
+  <DashBoard />
 
   <!-- <main>
     <TheWelcome />
