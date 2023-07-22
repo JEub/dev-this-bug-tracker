1383fa1abc7ab9ce642cfd9fe27ab9f2464a3c73
Charles Taylor <charles4312@comcast.net>
2023-07-22 12:43:56 -0400

    Created Basic Table + Styled Table

--- frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
@@ -4,20 +4,24 @@
 
 <template>
     <div>
-        <table>
-            <tr>
-                <td>
-                    <button>All</button>
-                </td>
-                <td>
-                    <button>Mine</button>
-                </td>
-                <td>
-                    <input placeholder="Search"/>
-                </td>
-                <td>
-                    <button>Filter</button>
-                </td>
+        <table class="dashboard-table">
+            <tr class="dashboard-header">
+                <div>
+                    <td>
+                        <button>All</button>
+                    </td>
+                    <td>
+                        <button>Mine</button>
+                    </td>
+                </div>
+                <div>
+                    <td>
+                        <input placeholder="Search"/>
+                    </td>
+                    <td>
+                        <button>Filter</button>
+                    </td>
+                </div>
             </tr>
             <!-- Create a mapping function to display tickets retrieved from the database -->
         </table>
@@ -26,5 +30,5 @@
 </template>
 
 <style>
-
+    @import './DashBoard.css';
 </style>
