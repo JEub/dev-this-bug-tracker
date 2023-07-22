4a665d6abf54192a2a51df04c8124a5557b3df89
Charles Taylor <charles4312@comcast.net>
2023-07-22 11:45:20 -0400

    Imported NavBar Component To HomeView

--- frontend/dev-this-bug-tracker-frontend/src/views/HomeView.vue
+++ frontend/dev-this-bug-tracker-frontend/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
+import NavBar from '../components/NavBar/NavBar.vue'
 </script>
 
 <template>
   <nav>
-    
+    <NavBar />
   </nav>
   <main>
     <!-- <TheWelcome /> -->
