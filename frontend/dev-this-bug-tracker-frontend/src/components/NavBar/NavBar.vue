26aaa1725b40b09de8fa35fa7f48391e54a6ed8d
Charles Taylor <charles4312@comcast.net>
2023-07-22 11:51:37 -0400

    Created Classes For NavBar Component For Flex Displays

--- frontend/dev-this-bug-tracker-frontend/src/components/NavBar/NavBar.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/NavBar/NavBar.vue
@@ -4,15 +4,17 @@
 
 <template>
     <!-- Temporary -->
-    <div>
-        <button>Create Ticket</button>
-        <button>Create User</button>
-    </div>
-    <div>
-        <h2>Welcome, User</h2>
-        <div>
-            <img />
-            <a>Logout</a>
+    <div class="navbar">
+        <div class="buttons-section">
+            <button>Create Ticket</button>
+            <button>Create User</button>
+        </div>
+        <div class="user-section">
+            <h2>Welcome, User</h2>
+            <div class="user-section-details">
+                <img />
+                <a>Logout</a>
+            </div>
         </div>
     </div>
 </template>
