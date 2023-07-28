66234fd3d58b0430df68c19c302c925fc7e4e36d
Charles Taylor <charles4312@comcast.net>
2023-07-28 18:56:54 -0400

    Reformated Table To Contain thead And tbody Tags For Acessability

--- frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
@@ -22,45 +22,30 @@ const searchTerm = ref('');
             </div>
         </div>
         <table class="dashboard-table">
-            <tr>
-                <!-- <div>
-                    <td>
-                        <button>All</button>
-                    </td>
-                    <td>
-                        <button>Mine</button>
-                    </td>
-                </div>
-                <div>
-                    <td>
-                        <input type='text' placeholder="Search" v-model="searchTerm"/>
-                    </td>
-                    <td>
-                        <button>Filter</button>
-                    </td>
-                </div> -->
-            </tr>
-            <tr>
-                <td>
-                    <h2>Id</h2>
-                </td>
-                <td>
-                    <h2>Title</h2>
-                </td>
-                <td>
-                    <h2>Assigned User</h2>
-                </td>
-                <td>
-                    <h2>Status</h2>
-                </td>
-                <td>
-                    <h2>Creation Date</h2>
-                </td>
-            </tr>
-            <tr v-for="ticket in tickets" :key="ticket.id">
-                <DashBoard_Table_Row v-if="ticket.id.includes(searchTerm) || ticket.title.includes(searchTerm) || ticket.assigned_user.username.includes(searchTerm) || ticket.created_date.includes(searchTerm)" :ticketData="ticket" />
-
-            </tr>
+            <thead>
+                <tr>
+                    <th>
+                        <h2>Id</h2>
+                    </th>
+                    <th>
+                        <h2>Title</h2>
+                    </th>
+                    <th>
+                        <h2>Assigned User</h2>
+                    </th>
+                    <th>
+                        <h2>Status</h2>
+                    </th>
+                    <th>
+                        <h2>Creation Date</h2>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="ticket in tickets" :key="ticket.id">
+                    <DashBoard_Table_Row v-if="ticket.id.includes(searchTerm) || ticket.title.includes(searchTerm) || ticket.assigned_user.username.includes(searchTerm) || ticket.created_date.includes(searchTerm)" :ticketData="ticket" />
+                </tr>
+            </tbody>
 
             <!-- Create a mapping function to display tickets retrieved from the database -->
         </table>
