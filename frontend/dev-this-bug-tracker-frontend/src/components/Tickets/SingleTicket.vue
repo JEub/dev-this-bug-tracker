815db6b72acd248472230be5c2f91248bfa1631c
Eric <ettrudell17@gmail.com>
2023-07-29 18:05:49 -0600

    polished up css for single ticket view modal

--- frontend/dev-this-bug-tracker-frontend/src/components/Tickets/SingleTicket.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/Tickets/SingleTicket.vue
@@ -16,37 +16,39 @@
 
 <template>
     <!-- pull data from database -->
-    <div class="body">
-        <div class="header">
-                <h3>Ticket id</h3>
-                <h3>Creator contact email</h3>
-                <h3>Assigned users</h3>
-                <h3>Ticket status</h3>
-        </div>
+    <div id="wrapper">
         <div class="body">
-            <div class="row">
-                <p>Date opened / closed</p>
-            </div>
             <div class="header">
-                <p>Title</p>
-                <p>Description</p>
-            </div>
-            <div class="row">Groups</div>
-            <div class="row">Story points</div>
-            <div class="row">
-                <ul>
-                    <li>Parent ticket links</li>
-                </ul>
-            </div>
-            <div class="row">
-                <ul>
-                    <li>History of assigned users w/time worked on ticket</li>
-                </ul>
-            </div>
-            <div class="row">
-                <p>Ticket tags</p>
-            </div>
-            <div class="body">
+                    <h3>Ticket id</h3>
+                    <h3>Creator contact email</h3>
+                    <h3>Assigned users</h3>
+                    <h3>Ticket status</h3>
+                    <button class="exit">Close</button>
+                </div>
+            <div class="content">
+                
+                <div class="row">
+                    <p>Date opened / closed</p>
+                </div>
+                <div class="header">
+                    <p>Title</p>
+                    <p>Description</p>
+                </div>
+                <div class="row">Groups</div>
+                <div class="row">Story points</div>
+                <div class="row">
+                    <ul>
+                        <li>Parent ticket links</li>
+                    </ul>
+                </div>
+                <div class="row">
+                    <ul>
+                        <li>History of assigned users w/time worked on ticket</li>
+                    </ul>
+                </div>
+                <div class="row">
+                    <p>Ticket tags</p>
+                </div>
                 <div class="row bigRow">
                     <button class="button"><a href="">Add Comment</a></button>
                     <div class="hide">
@@ -60,19 +62,12 @@
                         <p>Ticket comments 8</p>
                         <p>Ticket comments 9</p>
                         <p>Ticket comments 10</p>
-                        <p>Ticket comments 11</p>
-                        <p>Ticket comments 12</p>
-                        <p>Ticket comments 13</p>
-                        <p>Ticket comments 14</p>
-                        <p>Ticket comments 15</p>
-                        <p>Ticket comments 16</p>
                     </div>
                 </div>
-            </div>
-            <div class="header">
-            <!-- both of these buttons could open new modal? -->
-                
-                <button class="button"><a href="">Edit Ticket</a></button>
+                <div class="header">
+                <!-- both of these buttons could open new modal? -->
+                    <button class="button"><a href="">Edit Ticket</a></button>
+                </div>
             </div>
         </div>
     </div>
