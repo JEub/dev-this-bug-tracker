d0feb60715a1de6e38e53db54e01d5390f2baa5a
Eric <ettrudell17@gmail.com>
2023-08-05 15:05:36 -0600

    added open close modal functionality to fake open modal and real create edit ticket modal

--- frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
@@ -1,41 +1,58 @@
 <script setup>
 import { ref } from 'vue';
-
 import tickets from './tickets.json';
-import CreateEditTicket from '../Tickets/CreateEditTicket.vue'
+import CreateEditTicket from '../Tickets/CreateEditTicket.vue';
+import TicketModal from '../Tickets/TicketModal.vue';
 // import users from './users.json';
 import DashBoard_Table_Row from './DashBoard_Table_Row/DashBoard_Table_Row.vue';
 
 const searchTerm = ref('');
-
+const isOpen = ref(false)
+const createOpen = ref(false)
+// ##### added ######
+const toggleModal = () => {
+    alert( 'You toggled open modal' );
+    isOpen.value = !isOpen.value;
+    return toggleModal;
+};
+const createToggle = () => {
+    alert( 'You toggled create modal' );
+    createOpen.value = !createOpen.value;
+    return createToggle;
+};
+// ######
 </script>
 
 <script>
     export default {
-    name: 'DashBoard',
-    components: {
-        CreateEditTicket,
-    },
-    data() {
-        return {
-        modalData: null,
-        isModalVisible: false,
-        };
-    },
-    methods: {
-        showModal(ticketData) {
-            console.log('test?')
-            this.modalData = ticketData;
-            this.isModalVisible = true;
+        name: 'DashBoard',
+        components: {
+            CreateEditTicket,
+            TicketModal
         },
-        closeModal() {
-            this.isModalVisible = false;
-        }
-    }
-};
+        data () {
+            return {
+                modalData: null,
+                isModalVisible: false,
+            };
+        },
+        methods: {
+            showModal(ticketData) {
+                console.log('test?');
+                alert( 'You opened the modal from dashboard' );
+                this.modalData = ticketData;
+                this.isModalVisible = true;
+            },
+            closeModal() {
+                this.isModalVisible = false;
+                alert( 'You closed the modal' );
+            }
+        },
+    };
 </script>
 
 <template>
+    
     <div>
         <div class="dashboard-header">
             <div>
@@ -74,22 +91,40 @@ const searchTerm = ref('');
                     />
                 </tr>
             </tbody>
-
-
-
             <!-- Create a mapping function to display tickets retrieved from the database -->
         </table>
-
-        
     </div>
-    <!--@click="isModalVisable=true"-->
-    <CreateEditTicket
-        v-show="isModalVisible"
+    <!--@click="isModalVisable=true"
         @close="closeModal"
         class="modal-popup"
-        :ticket="modalData"
+        :ticket="modalData" -->
+    <CreateEditTicket
+        v-show="isModalVisible"
+        @click="toggleModal"
     />
-    
+    <!--### ADDED ###-->
+    <div class="root" >
+        <button @click="createToggle">Create Ticket</button>
+        <teleport to="body">
+            <div class="modal" v-if="createOpen">
+                <!-- note: change onclikc to on close so form input can be clocked without closing modal-->
+                <CreateEditTicket
+                    @click="createToggle"
+                    title="Create / Edit Ticket"
+                />
+            </div>
+        </teleport>
+        <button @click="toggleModal">Open</button>
+        <teleport to="body">
+            <div class="modal" v-if="isOpen">
+                <TicketModal
+                    @click="toggleModal"
+                    title="Does this work?"
+                    msg="I hope so"
+                />
+            </div>
+        </teleport>
+    </div>
 </template>
 
 <style>
