800f72d2c2ab2f8fe7fac6b9a421bb29b8595266
Charles Taylor <charles4312@comcast.net>
2023-08-09 21:09:28 -0400

    Updated Styles For Pagination Buttons

--- frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
+++ frontend/dev-this-bug-tracker-frontend/src/components/DashBoard/DashBoard.vue
@@ -276,16 +276,17 @@ import DashBoard_Table_Row from './DashBoard_Table_Row/DashBoard_Table_Row.vue';
         </div>
 
         
-        <nav>
+        <nav class="pagination-container">
             <div class="pagination">
-                <button @click="prevPage" :class="{ disabled: currentPage === 1 }">Previous</button>
+                <button @click="prevPage" class="pagination-button" :class="{ disabled: currentPage === 1 }">Previous</button>
                 <button v-for="pageNumber in totalPages"
                     :key="pageNumber"
                     @click="gotoPage(pageNumber)"
-                    :class="{ active: currentPage === pageNumber }">
+                    :class="{ active: currentPage === pageNumber }"
+                    class="pagination-button" >
                 {{ pageNumber }}
                 </button>
-                <button @click="nextPage" :class="{ disabled: currentPage === totalPages }">Next</button>
+                <button @click="nextPage" class="pagination-button" :class="{ disabled: currentPage === totalPages }">Next</button>
             </div>
         </nav>
     </div>
