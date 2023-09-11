9b60304eb43a15f0f89e56a410f79ea7ece1f053
Taeyong Lee <d104601@hotmail.com>
2023-09-10 23:27:22 -0400

    updating main

--- backend/config/database.py
+++ backend/config/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Need to add DB URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+# Need to add DB URL(postgres in docker container)
+SQLALCHEMY_DATABASE_URL = 'postgresql://user:password@db:5432/bugtracker'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- backend/model/group.py
+++ backend/model/group.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
-from database import Base
+from config.database import Base
 
 
 class Group(Base):
